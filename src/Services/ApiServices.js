bf9774b895e3989c4b070e3f83e6b8083ee6b58b
Alberto Jimenez Gonzalez <alberjghoyos@gmail.com>
2026-03-23 19:45:58 +0100

    add try and catch to API calls

--- src/Services/ApiServices.js
+++ src/Services/ApiServices.js
@@ -1,54 +1,86 @@
 const API_URL = import.meta.env.VITE_API_URL;
 async function getCryptosList() {
-  const response = await fetch(
-    `${API_URL}${import.meta.env.VITE_API_URL_ALL_COINS}`,
-    {
-      headers: {
-        "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
+  try {
+    const response = await fetch(
+      `${API_URL}${import.meta.env.VITE_API_URL_ALL_COINS}`,
+      {
+        headers: {
+          "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
+        },
       },
-    },
-  );
-  const data = await response.json();
-  return data;
+    );
+
+    if (!response.ok) {
+      throw new Error(`Errooor ${response.status}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener la lista de criptomondedas,", error);
+    throw error;
+  }
 }
 
 async function getCoinById(id) {
-  const response = await fetch(`${API_URL}${id}`, {
-    method: "GET",
-    headers: {
-      "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
-    },
-  });
-  const data = await response.json();
-  return data;
-}
-
-async function getCurrencyPricesPerDayById(id, days) {
-  const response = await fetch(
-    `${API_URL}${id}${import.meta.env.VITE_API_URL_COIN_DETAILS}${days}`,
-    {
+  try {
+    const response = await fetch(`${API_URL}${id}`, {
       method: "GET",
       headers: {
         "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
       },
-    },
-  );
-  const data = await response.json();
-  return data;
+    });
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener la informacion de la criptomoneda,", error);
+    throw error;
+  }
+}
+
+async function getCurrencyPricesPerDayById(id, days) {
+  try {
+    const response = await fetch(
+      `${API_URL}${id}${import.meta.env.VITE_API_URL_COIN_DETAILS}${days}`,
+      {
+        method: "GET",
+        headers: {
+          "x-cg-demo-api-key": import.meta.env.VITE_API_KEY,
+        },
+      },
+    );
+    if (!response.ok) {
+      throw new Error(response.status);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error al obtener los datos de la grafica,", error);
+    throw error;
+  }
 }
 
 async function translateText(text) {
   if (!text) return "";
-  const response = await fetch(
-    `${import.meta.env.VITE_API_TRANSLATE_URL}${encodeURIComponent(text)}`,
-  );
-  const data = await response.json();
-  const refactorText = data.translation;
-
-  return refactorText;
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_TRANSLATE_URL}${encodeURIComponent(text)}`,
+    );
+    if(!response.ok){
+      throw new Error(response.status)
+    }
+    const data = await response.json();
+    const refactorText = data.translation;
+    return refactorText;
+  } catch (error) {
+    console.error("Error al traducir la descripción de la criptomoneda,", error)
+    throw error
+  }
 }
 
-export{
+export {
   getCryptosList,
   getCoinById,
   getCurrencyPricesPerDayById,
